ebdd0bcfd5fd390f94ff2d013fe1a6e1d8581a89
Junseok Oh <kotohana5706@edcan.kr>
2015-11-30 00:58:06 +0900

    Working : AuthActivity

---
+++ app/src/main/java/malang/moe/repay/activity/AuthActivity.java
@@ -0,0 +1,15 @@
+package malang.moe.repay.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import malang.moe.repay.R;
+
+public class AuthActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
